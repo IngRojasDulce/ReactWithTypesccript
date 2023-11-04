88eec9a10c5534218e78377dbabdaeb778452351
Dulce rojas <dulcerojas698@gmail.com>
2023-11-03 23:36:35 -0300

    pequeño ejemplo de asignaciones en TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [number, setNumber] = useState(5) // aqui inicia munero
+  const changeNumber = ()=> {
+    setNumber ("w")// no me deja asignar otro valor que no sea numero
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button></button>
     </div>
   );
 }

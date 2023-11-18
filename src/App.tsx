13de2faead24ae830b1b080865084705bfb7b608
Dulce rojas <dulcerojas698@gmail.com>
2023-11-18 00:29:32 -0300

    aplicando un pequeño estado

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const [subs, setSubs] = useState([
-    {
+
+const INITIAL_STATE=[
+  {
       nick: 'nombre',
       subMonths: 3,
       avatar : 'https://i.pravatar.cc/150?u=dapelu',
@@ -16,8 +16,37 @@ function App() {
       avatar : 'https://i.pravatar.cc/150?u=dulce',
 
     }
+
+]
+
+interface AppState{// recomendacion hacer como una interfaz y adentro poenr cada uno de los estados del componente
+subs: Array<Sub>
+newSubsNumber: number
+}
+// declaracion de interface para generalizar los tipos de variables ts
+interface Sub{
+  nick: string
+  avatar: string
+  subMonths: number
+  description?: string
+}
+ 
+
+
+
+
+function App() {
+  const [subs, setSubs] = useState<AppState["subs"]>([])// Estoy diciendo que es un array de sub especificando las variables declaradas en interface
+  const [newSubsNumber, setNewSubsNumber] = useState<AppState["newSubsNumber"]>(0)
+  
+  useEffect(()=>
+  {
+    setSubs(INITIAL_STATE)
+  },[]
+)
+    //
     
-  ]) // aqui inicia munero
+   // aqui inicia munero
   // const changeNumber = ()=> {
   //   setNumber ([])// no me deja asignar otro valor que no sea numero
   // }

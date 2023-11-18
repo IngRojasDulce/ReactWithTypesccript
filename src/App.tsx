6391937c8d852d8fd25b1231d64571bfd91ea188
Dulce rojas <dulcerojas698@gmail.com>
2023-11-17 23:55:01 -0300

    armando carta

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [number, setNumber] = useState(5) // aqui inicia munero
-  const changeNumber = ()=> {
-    setNumber ("w")// no me deja asignar otro valor que no sea numero
-  }
+  const [subs, setSubs] = useState([
+    {
+      nick: 'nombre',
+      subMonths: 3,
+      avatar : 'https://i.pravatar.cc/150?u=dapelu',
+      description: 'Esto es una descripcion '
+    },
+    {
+      nick: 'Dulce',
+      subMonths: 8,
+      avatar : 'https://i.pravatar.cc/150?u=dulce',
+
+    }
+    
+  ]) // aqui inicia munero
+  // const changeNumber = ()=> {
+  //   setNumber ([])// no me deja asignar otro valor que no sea numero
+  // }
   return (
     <div className="App">
-      <button></button>
+      <h1> Dulce con TypeScript</h1>
+      <ul>
+        {subs.map(sub=>{
+          return (
+            <li key= {sub.nick}>
+              <img src= {sub.avatar} alt={`Avatar for ${sub.nick}`}/>
+              <h4>{sub.nick} (<small>{sub.subMonths}</small>)</h4>
+              <p>{sub.description?.substring(0,100)}</p>
+            </li>
+          )
+        })}
+      </ul>
+      
     </div>
   );
 }
